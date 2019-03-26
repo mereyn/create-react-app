7cc3dd8bc15612d0e9f55b578b6037ebc2d8a39a
Neza Melencion <mmelencion@gmail.com>
2019-03-26 22:50:49 +0800

    Updated home page

--- packages/react-scripts/template/src/components/pages/Home/HomePage.js
+++ packages/react-scripts/template/src/components/pages/Home/HomePage.js
@@ -1,37 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './_home.css';
+import React, { Component, Fragment } from "react";
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
 
 class HomePage extends Component {
-
-  getEnvValues() {
-    if (!process.env.REACT_APP_USER_ID || !process.env.REACT_APP_ID) {
-      throw new Error('Please define `REACT_APP_USER_ID` and `REACT_APP_ID` in your .env file');
-    }
-
-    const userID = process.env.REACT_APP_USER_ID;
-    const appID = process.env.REACT_APP_ID;
-
-    return { userID, appID };
-  }
-
-  render() {
-
-    const { userID, appID } = this.getEnvValues();
+  render () {
+    const { classes } = this.props;
 
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Neztek</h2>
-        </div>
-        <p className="App-intro">
-           <b> Employee ID: { userID } </b><br/><br/>
-           <b> Application ID: { appID } </b>
-        </p>
-      </div>
+      <Fragment>
+        <CssBaseline />
+        <AppBar position="static" className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              Neztek
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          {/* Hero unit */}
+          <div className={classes.heroUnit}>
+            <div className={classes.heroContent}>
+              <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                About Us
+              </Typography>
+              <Typography variant="h6" align="center" color="textSecondary" paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Typography>
+              <div className={classes.heroButtons}>
+                <Grid container spacing={16} justify="center">
+                  <Grid item>
+                    <Button variant="contained" color="primary">
+                      Home
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button variant="outlined" color="primary" component={Link} to="/sign-in">
+                      Sign in
+                    </Button>
+                  </Grid>
+                </Grid>
+              </div>
+            </div>
+          </div>
+        </main>
+        {/* Footer */}
+        <footer className={classes.footer}>
+          <Typography variant="h6" align="center" gutterBottom>
+            Footer
+          </Typography>
+          <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+            Something here to give the footer a purpose!
+          </Typography>
+        </footer>
+        {/* End footer */}
+      </Fragment>
     );
   }
+
 }
 
-export default HomePage;
+HomePage.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(HomePage);
